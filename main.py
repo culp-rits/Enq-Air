938bf621b00d8e485950906a7b6851f919a2ff7b
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-03-24 22:25:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from modules import *           #importing all the data structures, functions and data
 
-if __name__=="__main__":        #main funtion
+if __name__=="__main__":       
     add_data()                  #adding the pre-created user account
     start()                     #start app
